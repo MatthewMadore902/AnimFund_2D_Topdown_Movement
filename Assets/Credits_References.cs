bada74526785ee5ce657186ef6910fe2502c3b70
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2022-02-28 10:27:42 -0400

    Initial commit

---
+++ Assets/Credits_References.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Brackeys Tutorial https://www.youtube.com/watch?v=whzomFgjT50
+// Tiny RPG Forest Pack https://assetstore.unity.com/packages/2d/characters/tiny-rpg-forest-114685
+// Hundred Fires Games https://www.youtube.com/watch?v=VsSq_Ispo3Q
---
+++ Assets/Scripts/PlayerMovement_2D.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_2D : MonoBehaviour
+{   
+    [Header("Input settings:")]
+    public float speedMultiplier = 5.0f;
+
+    [Space]
+    [Header("Character Stats:")]
+    public Vector2 movementDirection;
+    public float movementSpeed;
+
+    [Space]
+    [Header("References:")]
+    public Rigidbody2D rb;
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+        Move();
+        Animate();
+
+        //Sets the idle to the last direction moved
+        if (Input.GetAxis("Horizontal") >= 0.1f || Input.GetAxis("Horizontal") <= -0.1f || Input.GetAxis("Vertical") >= 0.1f || Input.GetAxis("Vertical") <= -0.1f)
+        {
+            animator.SetFloat("LastMoveX", Input.GetAxis("Horizontal"));
+            animator.SetFloat("LastMoveY", Input.GetAxis("Vertical"));
+        }
+
+
+    }
+
+    void Move()
+    {
+        rb.velocity = movementDirection * movementSpeed * speedMultiplier;
+    }
+
+    void Animate()
+    {
+        animator.SetFloat("Horizontal", movementDirection.x);
+        animator.SetFloat("Vertical", movementDirection.y);
+        animator.SetFloat("Speed", movementSpeed);
+    }
+
+}
